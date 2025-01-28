1ffaf56a21527d056428d79e5eeaf09fe2dc424d
rain44556 <bristy44556@gmail.com>
2025-01-28 17:23:45 +0600

    Secure admin dashboard with authentication and added admin routes

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,23 @@
+import useAdmin from '@/hooks/useAdmin';
+import { AuthContext } from '@/provider/AuthProvider';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const AdminRoute = () => {
+
+    const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    const [admin, loadingAdmin] = useAdmin();
+
+    if (loading || loadingAdmin) {
+        return <progress className="progress w-48"></progress>
+    }
+
+    if (user && admin) {
+        return children;
+    }
+
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
+};
+
+export default AdminRoute;
