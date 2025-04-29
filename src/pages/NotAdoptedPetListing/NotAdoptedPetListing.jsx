f24f512dedade80600df6632942ef19a117cfc12
rain44556 <bristy44556@gmail.com>
2025-04-29 15:05:26 +0600

    style: update pet listing to responsive 3-4 column grid layout

--- src/pages/NotAdoptedPetListing/NotAdoptedPetListing.jsx
+++ src/pages/NotAdoptedPetListing/NotAdoptedPetListing.jsx
@@ -81,7 +81,7 @@ const NotAdoptedPetListing = () => {
                 </div>
             </div>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-6 my-10'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-6 my-10'>
                 {
                     selectedCategory ?
                         (filteredPets.length > 0 ?
